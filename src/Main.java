c43ee6e527528d9f84286dd48b703c1c1d424cc5
djdw04 <djdw04@student.cis.edu.hk>
2023-09-07 14:08:33 +0800

    Lab

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+// then press Enter. You can now see whitespace characters in your cod
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        import java.util.Scanner;
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println("Enter your age: ");
+            Scanner scanner = new Scanner(System.in);
+            int age = scanner.nextInt();
+            int dateAge = (age-2);
+            System.out.println(age + "-year olds should date somebody who is at least " + dateAge + "-years old.");
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
         }
     }
-}
